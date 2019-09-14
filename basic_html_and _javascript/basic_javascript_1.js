db8dc4cf2d7b92757e6c213b4a9433a984e982a6
ty0022 <38741629+ty0022@users.noreply.github.com>
2019-09-14 05:49:34 -0700

    Add files via upload

---
+++ basic_html_and _javascript/basic_javascript_1.js
@@ -0,0 +1,7 @@
+alert("hello, world!")
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish";
+		var result = String.fontcolor("green"); 
+		document.getElementById("Irish").inner.HTML = 
+		result;
+}
